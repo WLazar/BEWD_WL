66ac1a4f599a07b03c283ee5a2428303064f48fa
wlazar <whlazar@gmail.com>
2014-06-19 20:47:24 -0700

    adding shirts_app with update features

---
+++ shirts_app/app/controllers/shirts_controller.rb
@@ -0,0 +1,38 @@
+class ShirtsController < ApplicationController
+	def index
+  		@shirts = Shirt.search_for(params[:q])
+  	end
+
+	def show
+		@shirt = Shirt.find(params[:id])
+	end
+
+	def new
+		@shirt = Shirt.new
+	end
+
+	def create
+		@shirt = Shirt.new(shirt_params)
+		if @shirt.save
+			redirect_to @shirt
+		else
+			render 'new'
+		end
+	end
+
+	def update
+		@shirt = Shirt.find(params[:id])
+		@shirt.update shirt_params
+		redirect_to @shirt
+	end
+
+	def edit
+		@shirt = Shirt.find(params[:id])
+	end
+
+	private
+		def shirt_params
+			params.require('shirt').permit(:name, :description, :image)
+		end
+
+end
---
+++ shirts_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ShirtsApp::Application.config.secret_key_base = '01e334089fe05a8e22cfba6987e0500361cc31c93a0f74d553c8fc0724b0afc187a2d560a9679eae88b1c579032a4dd8a81bd38df71061c31a025a4d0210e3bc'
